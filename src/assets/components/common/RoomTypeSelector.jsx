793aabfcb6f19739b7a3ce728d789b27a76a1794
Sekou S Seh <sehsekou98@gmail.com>
2024-03-01 13:37:02 +0100

    add room

---
+++ src/assets/components/common/RoomTypeSelector.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+import { getRoomTypes } from '../utilis/ApiFunctions'
+
+// eslint-disable-next-line react/prop-types
+const RoomTypeSelector = ({ handleRoomInputChange, newRoom }) => {
+    const [roomTypes, setRoomTypes] = useState([""])
+    const [showNewRoomTypeInput, setShowNewRoomTypeInput] = useState(false)
+    const [newRoomType, setNewRoomType] = useState("")
+
+    useEffect(() => {
+        getRoomTypes().then((data) => { setRoomTypes(data) })
+    }, [])
+
+    const handleNewRoomTypeInputChange = (e) => {
+        setNewRoomType(e.target.value)
+    }
+
+    const handleAddNewRoomType = () => {
+        if (newRoomType !== "") {
+            setRoomTypes([...roomTypes, newRoomType])
+            setNewRoomType("")
+            setShowNewRoomTypeInput(false)
+        }
+    }
+
+    return (
+        <>
+            {roomTypes.length > 0 && (
+                <div>
+                    <select
+                        id='roomType'
+                        name='roomType'
+                        // eslint-disable-next-line react/prop-types
+                        value={newRoom.roomType}
+                        onChange={(e) => {
+                            if (e.target.value === "Add New") {
+                                setShowNewRoomTypeInput(true)
+                            } else {
+                                handleRoomInputChange(e)
+                            }
+                        }}>
+                        <option value={""}>select a room type</option>
+                        <option value={"Add New"}>Add New</option>
+                        {roomTypes.map((type, index) => (
+                            <option key={index} value={type}>{type}</option>
+                        ))}
+                    </select>
+
+                    {showNewRoomTypeInput && (
+                        <div className='input-group'>
+                            <input
+                                className='form-control'
+                                type='text'
+                                placeholder='Enter a new room type'
+                                value={newRoomType}
+                                onChange={handleNewRoomTypeInputChange}
+                            />
+                            <button className='btn btn-hotel' type='button'
+                                onClick={handleAddNewRoomType}>
+                                Add
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )}
+        </>
+    )
+}
+
+export default RoomTypeSelector
--- src/assets/components/room/AddRoom.jsx
+++ src/assets/components/room/AddRoom.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import { useState } from 'react';
+import { addRoom } from '../utilis/ApiFunctions';
 
 const AddRoom = () => {
-  return (
-    <div>AddRoom</div>
-  )
-}
+  const [newRoom, setNewRoom] = useState({
+    photo: null,
+    roomType: '',
+    roomPrice: ''
+  });
 
-export default AddRoom
+  const [imagePreview, setImagePreview] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [successMessage, setSuccessMessage] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleRoomInputChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    if (name === 'roomPrice') {
+      // Parse roomPrice to integer
+      value = !isNaN(value) ? parseInt(value) : '';
+    }
+    setNewRoom({ ...newRoom, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setNewRoom({ ...newRoom, photo: selectedImage });
+    setImagePreview(URL.createObjectURL(selectedImage));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice);
+      setSuccessMessage('Successfully added new room to the database.');
+      setNewRoom({ photo: null, roomType: '', roomPrice: '' });
+      setImagePreview('');
+      setErrorMessage('');
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <>
+      <section className='container mt-5'>
+        <div className='row justify-content-center'>
+          <div className='col-md-8 col-lg-6'>
+            <h2 className='mt-5 mb-2'>Add a New Room</h2>
+            <form onSubmit={handleSubmit}>
+              <div className='mb-3'>
+                <label htmlFor='roomType' className='form-label'>
+                  Room Type
+                </label>
+                <input
+                  className='form-control'
+                  id='roomType'
+                  type='text'
+                  name='roomType'
+                  value={newRoom.roomType}
+                  onChange={handleRoomInputChange}
+                />
+              </div>
+
+              <div className='mb-3'>
+                <label htmlFor='roomPrice' className='form-label'>
+                  Room Price
+                </label>
+                <input
+                  className='form-control'
+                  required
+                  id='roomPrice'
+                  type='number'
+                  name='roomPrice'
+                  value={newRoom.roomPrice}
+                  onChange={handleRoomInputChange}
+                />
+              </div>
+
+              <div className='mb-3'>
+                <label htmlFor='photo' className='form-label'>
+                  Room photo
+                </label>
+                <input
+                  id='photo'
+                  name='photo'
+                  type='file'
+                  className='form-control'
+                  onChange={handleImageChange}
+                />
+                {imagePreview && (
+                  <img
+                    src={imagePreview}
+                    alt='preview'
+                    style={{ maxWidth: '400px', maxHeight: '400px' }}
+                    className='mb-3'
+                  />
+                )}
+              </div>
+
+              <div className='d-grid d-md-flex mt-2'>
+                <button className='btn btn-outline-primary ml-5' type='submit'>
+                  Save Room
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AddRoom;
