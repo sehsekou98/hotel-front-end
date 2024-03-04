5468b090442738d5b86bba3330d5bb5039afac11
Sekou S Seh <sehsekou98@gmail.com>
2024-03-04 17:55:43 +0100

    update

--- src/components/booking/BookingSummary.jsx
+++ src/components/booking/BookingSummary.jsx
@@ -1,96 +1,111 @@
-import  { useState, useEffect } from "react"
-import moment from "moment"
-import Button from "react-bootstrap/Button"
-import { useNavigate } from "react-router-dom"
+import  { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+import moment from "moment";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
-	const checkInDate = moment(booking.checkInDate)
-	const checkOutDate = moment(booking.checkOutDate)
-	const numberOfDays = checkOutDate.diff(checkInDate, "days")
-	const [isBookingConfirmed, setIsBookingConfirmed] = useState(false)
-	const [isProcessingPayment, setIsProcessingPayment] = useState(false)
-	const navigate = useNavigate()
+    const checkInDate = moment(booking.checkInDate);
+    const checkOutDate = moment(booking.checkOutDate);
+    const numberOfDays = checkOutDate.diff(checkInDate, "days");
+    const [isBookingConfirmed, setIsBookingConfirmed] = useState(false);
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+    const navigate = useNavigate();
 
-	const handleConfirmBooking = () => {
-		setIsProcessingPayment(true)
-		setTimeout(() => {
-			setIsProcessingPayment(false)
-			setIsBookingConfirmed(true)
-			onConfirm()
-		}, 3000)
-	}
+    const handleConfirmBooking = () => {
+        setIsProcessingPayment(true);
+        setTimeout(() => {
+            setIsProcessingPayment(false);
+            setIsBookingConfirmed(true);
+            onConfirm();
+        }, 3000);
+    };
 
-	useEffect(() => {
-		if (isBookingConfirmed) {
-			navigate("/booking-success")
-		}
-	}, [isBookingConfirmed, navigate])
+    useEffect(() => {
+        if (isBookingConfirmed) {
+            navigate("/booking-success");
+        }
+    }, [isBookingConfirmed, navigate]);
 
-	return (
-		<div className="row">
-			<div className="col-md-6"></div>
-			<div className="card card-body mt-5">
-				<h4 className="card-title hotel-color">Reservation Summary</h4>
-				<p>
-					Name: <strong>{booking.guestFullName}</strong>
-				</p>
-				<p>
-					Email: <strong>{booking.guestEmail}</strong>
-				</p>
-				<p>
-					Check-in Date: <strong>{moment(booking.checkInDate).format("MMM Do YYYY")}</strong>
-				</p>
-				<p>
-					Check-out Date: <strong>{moment(booking.checkOutDate).format("MMM Do YYYY")}</strong>
-				</p>
-				<p>
-					Number of Days Booked: <strong>{numberOfDays}</strong>
-				</p>
+    return (
+        <div className="row">
+            <div className="col-md-6"></div>
+            <div className="card card-body mt-5">
+                <h4 className="card-title hotel-color">Reservation Summary</h4>
+                <p>
+                    Name: <strong>{booking.guestFullName}</strong>
+                </p>
+                <p>
+                    Email: <strong>{booking.guestEmail}</strong>
+                </p>
+                <p>
+                    Check-in Date: <strong>{moment(booking.checkInDate).format("MMM Do YYYY")}</strong>
+                </p>
+                <p>
+                    Check-out Date: <strong>{moment(booking.checkOutDate).format("MMM Do YYYY")}</strong>
+                </p>
+                <p>
+                    Number of Days Booked: <strong>{numberOfDays}</strong>
+                </p>
 
-				<div>
-					<h5 className="hotel-color">Number of Guest</h5>
-					<strong>
-						Adult{booking.numOfAdults > 1 ? "s" : ""} : {booking.numOfAdults}
-					</strong>
-					<strong>
-						<p>Children : {booking.numOfChildren}</p>
-					</strong>
-				</div>
+                <div>
+                    <h5 className="hotel-color">Number of Guest</h5>
+                    <strong>
+                        Adult{booking.numOfAdults > 1 ? "s" : ""} : {booking.numOfAdults}
+                    </strong>
+                    <strong>
+                        <p>Children : {booking.numOfChildren}</p>
+                    </strong>
+                </div>
 
-				{payment > 0 ? (
-					<>
-						<p>
-							Total payment: <strong>${payment}</strong>
-						</p>
+                {payment > 0 ? (
+                    <>
+                        <p>
+                            Total payment: <strong>${payment}</strong>
+                        </p>
 
-						{isFormValid && !isBookingConfirmed ? (
-							<Button variant="success" onClick={handleConfirmBooking}>
-								{isProcessingPayment ? (
-									<>
-										<span
-											className="spinner-border spinner-border-sm mr-2"
-											role="status"
-											aria-hidden="true"></span>
-										Booking Confirmed, redirecting to payment...
-									</>
-								) : (
-									"Confirm Booking & proceed to payment"
-								)}
-							</Button>
-						) : isBookingConfirmed ? (
-							<div className="d-flex justify-content-center align-items-center">
-								<div className="spinner-border text-primary" role="status">
-									<span className="sr-only">Loading...</span>
-								</div>
-							</div>
-						) : null}
-					</>
-				) : (
-					<p className="text-danger">Check-out date must be after check-in date.</p>
-				)}
-			</div>
-		</div>
-	)
-}
+                        {isFormValid && !isBookingConfirmed ? (
+                            <Button variant="success" onClick={handleConfirmBooking}>
+                                {isProcessingPayment ? (
+                                    <>
+                                        <span
+                                            className="spinner-border spinner-border-sm mr-2"
+                                            role="status"
+                                            aria-hidden="true"></span>
+                                        Booking Confirmed, redirecting to payment...
+                                    </>
+                                ) : (
+                                    "Confirm Booking & proceed to payment"
+                                )}
+                            </Button>
+                        ) : isBookingConfirmed ? (
+                            <div className="d-flex justify-content-center align-items-center">
+                                <div className="spinner-border text-primary" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        ) : null}
+                    </>
+                ) : (
+                    <p className="text-danger">Check-out date must be after check-in date.</p>
+                )}
+            </div>
+        </div>
+    );
+};
 
-export default BookingSummary
+BookingSummary.propTypes = {
+    booking: PropTypes.shape({
+        guestFullName: PropTypes.string.isRequired,
+        guestEmail: PropTypes.string.isRequired,
+        checkInDate: PropTypes.instanceOf(Date).isRequired,
+        checkOutDate: PropTypes.instanceOf(Date).isRequired,
+        numOfAdults: PropTypes.number.isRequired, 
+        numOfChildren: PropTypes.number.isRequired,
+    }).isRequired,
+    payment: PropTypes.number.isRequired,
+    isFormValid: PropTypes.bool.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+};
+
+export default BookingSummary;
