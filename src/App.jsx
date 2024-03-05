bc14d7b69b6723297a24e37c2370d5e609d72832
Sekou S Seh <sehsekou98@gmail.com>
2024-03-05 23:21:07 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import RoomListing from "./components/room/RoomListing"
 import Admin from "./components/admin/Admin"
 import Checkout from "./components/booking/Checkout"
 import BookingSuccess from "./components/booking/BookingSuccess"
+import Bookings from "./components/booking/Bookings"
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
 						<Route path="/browse-all-rooms" element={<RoomListing />} />
 						<Route path="/admin" element={<Admin/>} />
 						<Route path="/booking-success" element={<BookingSuccess/>} />
+						<Route path="/booking" element={<Bookings/>} />
 					</Routes>
 				</Router>
 				<Footer />
--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -5,7 +5,7 @@ const Admin = () => {
     <section className="container mt-5">
       <h2>Welcome to the Admin Panel.</h2>
       <hr/>
-      <Link to={"/rxisting-room"}>Manage Rooms</Link> <br/>
+      <Link to={"/rxisting-rooms"}>Manage Rooms</Link> <br/>
       <Link to={"/existing-bookings"}>Manage Bookings</Link>
     </section>
   )
